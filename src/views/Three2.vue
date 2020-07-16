4dbc7c3aeca046412f66eb8a61d2c4f4c07c009e
helong <helong@qq.com>
2020-07-16 11:49:45 +0800

    add: 水平移动、自由移动切换

--- src/views/Three2.vue
+++ src/views/Three2.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="container">
+  <section class="container">
     <div class="three"></div>
-    <button class="btn" @click="reset">重置</button>
-  </div>
+    <section>
+      <button class="btn" @click="reset">重置</button>
+      <button class="btn" @click="freeMove(false)">水平移动</button>
+      <button class="btn" @click="freeMove(true)">自由移动</button>
+    </section>
+  </section>
 </template>
 
 <script>
@@ -11,6 +15,7 @@ import img2k from '@/assets/2k_earth_daymap.jpg'
 const OrbitControls = require('three-orbit-controls')(THREE)
 
 let renderer, controls, camera
+let mesh
 
 export default {
   name: 'Three2',
@@ -21,21 +26,41 @@ export default {
     reset () {
       controls.reset()
     },
+    freeMove (type) {
+      if (type) {
+        // 自由移动.
+        controls.minPolarAngle = 0 // 你能够垂直旋转的角度的下限，范围是0到Math.PI，其默认值为0。
+        controls.maxPolarAngle = Math.PI // 你能够垂直旋转的角度的上限，范围是0到Math.PI，其默认值为Math.PI。
+      } else {
+        // 限制水平移动.
+        controls.minPolarAngle = Math.PI / 2 // 你能够垂直旋转的角度的下限，范围是0到Math.PI，其默认值为0。
+        controls.maxPolarAngle = Math.PI / 2 // 你能够垂直旋转的角度的上限，范围是0到Math.PI，其默认值为Math.PI。
+      }
+
+      controls.reset()
+    },
     async init () {
       /* 初始化渲染实例及设置 */
       renderer = new THREE.WebGLRenderer({
         alpha: true, // canvas是否包含alpha (透明度)。默认为 false
-        premultipliedAlpha: false, // renderer是否假设颜色有 premultiplied alpha. 默认为true
-        antialias: true // 抗锯齿
+        premultipliedAlpha: false // renderer是否假设颜色有 premultiplied alpha. 默认为true
+        // antialias: true // 抗锯齿
       })
       const size = Math.min(window.innerWidth, 600)
       renderer.setSize(size, size)
       renderer.setPixelRatio(window.devicePixelRatio)
 
+      /* 场景 */
+      const scene = new THREE.Scene()
+
       /* 相机 */
       camera = new THREE.PerspectiveCamera(45, 1, 0.1, 1000).translateZ(2.8)
+      // camera.position.set(1, 0, 2.8) // 设置摄像机的位置
+      // camera.lookAt(camera.position) // 设置摄像机观察的方向
       // camera.filmOffset = 100 // 水平偏离中心偏移量，和.filmGauge单位相同。默认值为0。
       camera.screenSpacePanning = false
+
+      /* 控制器 */
       controls = new OrbitControls(camera, renderer.domElement)
       controls.enableZoom = false // 启用或禁用摄像机的缩放。
       controls.maxZoom = 1 // 你能够将相机缩小多少
@@ -43,8 +68,8 @@ export default {
       controls.minDistance = 0 // 你能够将相机向内移动多少（仅适用于PerspectiveCamera），其默认值为0。
       controls.maxDistance = 10 // 你能够将相机向外移动多少（仅适用于PerspectiveCamera），其默认值为Infinity。
 
-      /* 场景 */
-      const scene = new THREE.Scene()
+      // controls.minAzimuthAngle = -Infinity //
+      // controls.maxAzimuthAngle = Infinity //
 
       /* 本地图片、网络获取图片 */
       const loader = new THREE.TextureLoader()
@@ -63,9 +88,14 @@ export default {
         }
       )
 
-      scene.add(new THREE.Mesh(new THREE.SphereBufferGeometry(1, 32, 32), new THREE.MeshBasicMaterial({
+      /* 材质 */
+      mesh = new THREE.Mesh(new THREE.SphereBufferGeometry(1, 32, 32), new THREE.MeshBasicMaterial({
         map
-      })))
+      }))
+      // mesh.rotation.x = 1
+      // mesh.rotation.y = 10
+      // mesh.rotation.z = 10
+      scene.add(mesh)
 
       /* 渲染 */
       setTimeout(function () {
@@ -103,10 +133,13 @@ export default {
   }
 
   .btn {
+    display: block;
     line-height: 40px;
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-bottom: 10px;
     padding-left: 1.5rem;
     padding-right: 1.5rem;
-    border: 1px solid #ccc;
     border-radius: 0.5rem;
     font-size: 16px;
     letter-spacing: 2px;
